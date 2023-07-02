897505ae20831fdfe6c9d473ece74a3ead99b43e
Ramachary <60394220+B151085@users.noreply.github.com>
2023-07-02 18:42:02 +0530

    final commit-- ready to serve
    dish is ready-- final commit

--- task1.js
+++ task1.js
@@ -3,3 +3,7 @@ console.log("first recipe");
 //adding ingredient to make first recipe more tastier
 
 comsole.log("first recipe added with ingredients");
+
+//serving the dish
+
+console.log("the dish is ready to eat");
